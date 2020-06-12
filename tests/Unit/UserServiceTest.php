d2863516610ea437d06e63ff942e65f3598f97f1
Osmell Caicedo <correo.oele@gmail.com>
2020-06-12 09:34:25 -0500

    test: add create update and delete test to user service test

--- tests/Unit/UserServiceTest.php
+++ tests/Unit/UserServiceTest.php
@@ -11,10 +11,18 @@ class UserServiceTest extends TestCase
 {
     protected $service;
 
-    public function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
         $this->service = app(UserService::class);
+        $this->faker   = \Faker\Factory::create();
+        $this->email   = $this->getFakeEmail();
+    }
+
+    protected function getFakeEmail()
+    {
+        return "issac53@instiguaimaral.edu.co";
+        // return $this->faker->userName .'@'. config('gsuite.admin-domain');
     }
 
     /** @test */
@@ -28,5 +36,47 @@ class UserServiceTest extends TestCase
     public function it_will_have_desired_service()
     {
         $this->assertInstanceOf(\Google_Service_Directory::class, $this->service->service);
+    }
+
+    /** @test */
+    public function impsersonate_user_is_defined()
+    {
+        $email = config('gsuite.service-account-impersonate');
+        $user  = $this->service->fetch($email);
+        $this->assertEquals($user->getEmail(), $email);
+    }
+
+
+    /** @test */
+    public function create_user()
+    {
+        $params = [
+            'email'     => $this->email,
+            'password'  => $this->faker->password,
+            'firstName' => $this->faker->firstName,
+            'lastName'  => $this->faker->lastName
+        ];
+
+        $user = $this->service->create($params);
+        $this->assertEquals($user->getEmail(), $params['email']);
+    }
+
+    /** @test  */
+    public function update_user_fullname()
+    {
+        $params =  [
+            'firstName' => $this->faker->firstName,
+            'lastName' => $this->faker->lastName
+        ];
+
+        $user = $this->service->updateName($this->email, $params);
+        $this->assertContains($user->getFullName(), implode(' ', $params));
+    }
+
+    /** @test */
+    public function deleted_user()
+    {
+        $user = $this->service->deleteUser($this->email);
+        $this->assertEmpty($user);
     }
 }

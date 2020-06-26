510d80774cbd3e5ed252dfd217828df153fc020e
Osmell Caicedo <correo.oele@gmail.com>
2020-06-26 15:57:05 -0500

    feat: refactor user service and add activate and suspend methods

--- src/Services/UserService.php
+++ src/Services/UserService.php
@@ -7,12 +7,14 @@ use Google_Service_Directory;
 use Google_Service_Directory_User;
 use Google_Service_Directory_UserName;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class UserService extends Service
 {
-    protected $name;
-    protected $joinedOn;
-    protected $designation;
+    protected $firstName;
+    protected $lastName;
+    protected $email;
+    protected $suspended;
 
     public function getServiceSpecificScopes(): array
     {
@@ -28,34 +30,14 @@ class UserService extends Service
         $this->service = new Google_Service_Directory($this->client);
     }
 
-    public function getUser(string $email)
-    {
-        return $this->service->users->get($email);
-    }
-
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    }
-
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    public function setFirstName($firstName)
-    {
-        $this->firstName = $firstName;
-    }
-
     public function getFirstName()
     {
         return $this->firstName;
     }
 
-    public function setLastName($lastName)
+    public function setFirstName(string $firstName)
     {
-        $this->lastName = $lastName;
+        $this->firstName = $firstName;
     }
 
     public function getLastName()
@@ -63,114 +45,179 @@ class UserService extends Service
         return $this->lastName;
     }
 
-    public function getFullName()
+    public function setLastName(string $lastName)
     {
-        return $this->getFirstName() . ' ' . $this->getLastName();
+        $this->lastName = $lastName;
     }
 
-    public function setUser(Google_Service_Directory_User $user)
+    public function getEmail()
     {
-        $this->setEmail($user->getPrimaryEmail());
-        $this->setFirstName($user->getName()->getGivenName());
-        $this->setLastName($user->getName()->getFamilyName());
+        return $this->email;
+    }
 
-        return $this;
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+    }
+
+    public function getSuspended()
+    {
+        return $this->suspended;
+    }
+
+    public function setSuspended(bool $suspended)
+    {
+        $this->suspended = $suspended;
     }
 
     public function fetch($email)
     {
-        $user = $this->getUser($email);
-        return  $this->setUser($user);
-    }
-
-    public function create(array $params)
-    {
-        $validator = Validator::make($params, [
-                'email'     => 'required|email',
-                'firstName' => 'required|min:3',
-                'lastName'  => 'required|min:3',
-                'password'  => 'required|min:8',
-            ]);
-
-        if ($validator->fails()) {
-            throw new Exception(
-                implode("\n", $validator->errors()->all()),
-                2
-            );
-        }
-
-        $nameInstance = new Google_Service_Directory_UserName;
-        $nameInstance->setGivenName($params['firstName']);
-        $nameInstance->setFamilyName($params['lastName']);
-
-        $userInstance = new Google_Service_Directory_User;
-        $userInstance->setName($nameInstance);
-        $userInstance->setPrimaryEmail($params['email']);
-        $userInstance->setHashFunction('MD5');
-        $userInstance->setPassword(hash("md5", $params['password']));
-
-        try {
-            $user = $this->service->users->insert($userInstance);
-            return $this->setUser($user);
-        } catch (\Google_Service_Exception $gse) {
-            throw new Exception('already exists a count with the email '. $params['email'], $gse->getCode());
-        }
-    }
-
-    public function updateName(string $email, array $params)
-    {
-        $validator = Validator::make($params, [
-            'firstName' => 'required|min:3',
-            'lastName' =>  'required|min:3',
-        ]);
-
-        if ($validator->fails()) {
-            throw new Exception(
-                implode("\n", $validator->errors()->all()),
-                2
-            );
-        }
-
-        $user = $this->getUser($email);
+        $user = $this->service->users->get($email);
         $name = $user->getName();
 
-        $name->setGivenName($params['firstName']);
-        $name->setFamilyName($params['lastName']);
+        $this->setFirstName($name->getGivenName());
+        $this->setLastName($name->getFamilyName());
+        $this->setEmail($user->getPrimaryEmail());
+        $this->setSuspended($user->getSuspended());
 
-        $user->setName($name);
+        return $this;
+    }
 
-        $updatedUser = $this->service->users->update($email, $user);
+    public function create(array $input)
+    {
+        try {
+            Validator::make(
+                $input,
+                [
+                    'email'     => 'required|email',
+                    'firstName' => 'required|min:3',
+                    'lastName'  => 'required|min:3',
+                    'password'  => 'required|min:8'
+                ]
+            )->validate();
 
-        return $this->setUser($updatedUser);
+            $this->setFirstName($input['firstName']);
+            $this->setLastName($input['lastName']);
+            $this->setEmail($input['email']);
+            $this->setSuspended($input['suspended'] ?? false);
+
+
+            $nameInstance = new Google_Service_Directory_UserName;
+            $nameInstance->setGivenName($this->getFirstName());
+            $nameInstance->setFamilyName($this->getLastName());
+
+            $userInstance = new Google_Service_Directory_User;
+            $userInstance->setName($nameInstance);
+            $userInstance->setPrimaryEmail($this->getEmail());
+            $userInstance->setHashFunction('MD5');
+            $userInstance->setPassword(hash("md5", $input['password']));
+
+            $this->service->users->insert($userInstance);
+
+            return $this;
+        } catch (ValidationException $v) {
+            $this->handleException($v);
+        } catch (\Google_Service_Exception $gse) {
+            throw new Exception('already exists a count with the email '. $input['email'], $gse->getCode());
+        }
+    }
+
+    public function updateName(string $email, array $input)
+    {
+        try {
+            Validator::make(
+                $input,
+                [
+                    'firstName' => 'required|min:3',
+                    'lastName' =>  'required|min:3',
+                ]
+            );
+
+            $this->fetch($email);
+            $this->setFirstName($input['firstName']);
+            $this->setLastName($input['lastName']);
+
+            $user = $this->service->users->get($this->getEmail());
+
+            $name = $user->getName();
+            $name->setGivenName($this->getFirstName());
+            $name->setFamilyName($this->getLastName());
+
+            $user->setName($name);
+
+            $this->service->users->update($email, $user);
+
+            return $this;
+        } catch (ValidationException $v) {
+            $this->handleException($v);
+        } catch (\Throwable $th) {
+            throw new Exception('Error to update account : '. $this->getEmail(), $th->getCode());
+        }
     }
 
     public function setPassword(string $email, string $password)
     {
-        $validator = Validator::make([
-            'password' => $password
-        ], [
-            'password' => 'required|min:8',
-        ]);
+        try {
+            Validator::make(['password' => $password ], [ 'password' => 'required|min:8' ]);
 
-        if ($validator->fails()) {
-            throw new Exception(
-                implode("\n", $validator->errors()->all()),
-                2
-            );
+            $this->fetch($email);
+            $user = $this->service->users->get($email);
+            $user->setHashFunction('MD5');
+            $user->setPassword(hash("md5", $password));
+
+            $this->service->users->update($email, $user);
+
+            return $this;
+        } catch (ValidationException $v) {
+            throw new Exception($v->getResponse(), $v->getCode());
         }
-
-        $user = $this->getUser($email);
-        $user->setHashFunction('MD5');
-        $user->setPassword(hash("md5", $password));
-
-        $updatedUser = $this->service->users->update($email, $user);
-
-        return $this->setUser($updatedUser);
     }
 
-    public function deleteUser(string $email)
+    public function activate(string $email)
     {
-        $response = $this->service->users->delete($email);
-        return $response->getBody()->getContents();
+        $this->fetch($email);
+
+        if (! $this->getSuspended()) {
+            return $this;
+        }
+
+        $this->setSuspended(false);
+        $user = $this->service->users->get($email);
+        $user->setSuspended($this->getSuspended());
+        $this->service->users->update($email, $user);
+
+        return $this;
+    }
+
+    public function suspend(string $email)
+    {
+        $this->fetch($email);
+
+        if ($this->getSuspended()) {
+            return $this;
+        }
+
+        $this->setSuspended(true);
+
+        $user = $this->service->users->get($email);
+        $user->setSuspended($this->getSuspended());
+        $this->service->users->update($email, $user);
+
+        return $this;
+    }
+
+    public function destroy(string $email)
+    {
+        $this->fetch($email);
+        $this->service->users->delete($email);
+
+        return;
+    }
+
+    protected function handleException(ValidationException $v)
+    {
+        $errors = print_r($v->errors(), true);
+
+        throw new Exception($errors, $v->getCode());
     }
 }
